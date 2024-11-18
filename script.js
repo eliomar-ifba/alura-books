4be2ea6f317aec4341e5340009917ec049225101
eliomar-ifba <eliomar.campos@ifba.edu.br>
2024-11-18 10:43:12 -0300

    menu suspenso com hover

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const menuItemCategoria = document.getElementById('menu2__itemCategoria');
 function ativarMenuLista(){ 
     menuLista.classList.toggle('active');
 }
-function ativarMenuLista2(){ 
-    menuLista2.classList.toggle('active');
-}
+
+// function ativarMenuLista2(){ 
+//     menuLista2.classList.toggle('active');
+// }
 
 menuImagem.addEventListener('click', ativarMenuLista);
-menuItemCategoria.addEventListener('click', ativarMenuLista2);
+// menuItemCategoria.addEventListener('mouseover', ativarMenuLista2);
